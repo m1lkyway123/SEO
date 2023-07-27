45ca0aafc55c50d2ecebb9a8ce50164caf65c14a
m1lkyway123 <124919438+m1lkyway123@users.noreply.github.com>
2023-07-27 13:24:05 +0300

    dev

---
+++ app/js/main.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("burger").addEventListener("click", function() {
+    document.querySelector("header").classList.toggle("open");
+    document.querySelector("body").classList.toggle("open");
+  });
+});
+
+$(function(){
+  new WOW().init();
+});
+
+// Найти все ссылки начинающиеся на #
+const anchors = document.querySelectorAll('a[href^="#"]')
+
+// Цикл по всем ссылкам
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault() // Предотвратить стандартное поведение ссылок
+    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    // Плавная прокрутка до элемента с id = href у ссылки
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
